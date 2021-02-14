363afe3998e361a13611f884964e1d7e7a9519f3
ProgramadorLeandroSantos <leandrosantosmartins1998@outlook.com>
2021-02-13 21:54:58 -0300

    some adjusts on clear history button

--- src/components/ClearHistorySearch/index.tsx
+++ src/components/ClearHistorySearch/index.tsx
@@ -25,6 +25,7 @@ const styles = StyleSheet.create({
         marginTop:10,
         flexDirection:"row",
         justifyContent: "center",
+        alignItems:'center',
     },
     text:{
         color:'black',
